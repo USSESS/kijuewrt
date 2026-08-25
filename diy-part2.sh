17db344f606108e460610b5a4cbf4f8981301a2b
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-25 16:56:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
+#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
 # This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-# ==========自定义固件信息 KiJueWrt ==========
-cat > package/base-files/files/etc/openwrt_release <<EOF
-DISTRIB_ID="KiJueWrt"
-DISTRIB_RELEASE="1.0.0.1"
-DISTRIB_DESCRIPTION="KiJueWrt    1.0.0.1"
-DISTRIB_TARGET="x86/64"
-DISTRIB_ARCH="x86_64"
-EOF
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 修改主机名
-sed -i 's/OpenWrt/KiJueWrt/g' package/base-files/files/bin/config_generate
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 修改默认后台IP为10.10.10.1
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-
-# 设置开机默认argon主题（22.03有效）
-sed -i '/uci set luci.main.mediaurlbase/a\uci set luci.main.theme=luci-theme-argon' package/base-files/files/bin/config_generate
-
-# 壁纸：刷完固件网页后台上传JPG，不要脚本下载
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
