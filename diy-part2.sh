1322cd6e34fd450ae0cec282cb714181e401713a
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-25 09:51:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@ sed -i "s/uci set system.@system\[0\].hostname='OpenWrt'/uci set system.@system[
 sed -i 's/uci set network.lan.ipaddr=.*/uci set network.lan.ipaddr='"'"'10.10.10.1'"'"'/' package/base-files/files/bin/config_generate
 
 # 编译内置背景壁纸，不需要就删除本行
-wget -O package/luci-theme-argon/htdocs/argon/img/bg1.jpg https://p3terx.com/images/avatar.jpg
+wget -O package/luci-theme-argon/htdocs/argon/img/bg1.jpg https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEZyo5qjPTUxnbyC7LbF5vmSOF9u3eSOAACFSgAAg-uaVRlN0GOJvxdPT0E.jpg
