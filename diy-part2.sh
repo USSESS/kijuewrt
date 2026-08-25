6c84b4dca57ec93a5b97842d260bb4cb2467ee06
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-25 17:03:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Argon主题源
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
+# fanchmwrt插件包源
+echo 'src-git fanchmwrt https://github.com/fanchmwrt/fanchmwrt-packages.git' >> feeds.conf.default
