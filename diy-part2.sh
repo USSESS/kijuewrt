ce61c96fd3a4cf877cdc81493632adaabb4bf560
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-25 20:16:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,16 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 # 修改主机名为 KiJueWrt
 sed -i "s/set system.@system\[-1\].hostname=.*/set system.@system[-1].hostname='KiJueWrt'/g" package/base-files/files/bin/config_generate
 
+# 设置SSH登录banner KiJueWrt
+cat > package/base-files/files/etc/banner <<'EOF'
+
+ K   K  iii  JJJJJJ   u   u  EEEEE  W   W  RRRR   TTTTT
+ K  K    i      J     u   u  E      W   W  R   R    T
+ KKK     i      J     u   u  EEEE   W W W  RRRR     T
+ K  K    i      J     u   u  E      W W W  R  R     T
+ K   K  iii   JJJJ    uuuuu  EEEEE   W W   R   R    T
+
+
+              KiJueWrt  25.05.1  
+
+EOF
