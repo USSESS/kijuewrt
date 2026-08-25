a1907d8013e68567880e2347fc5d30819b2118d8
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-25 08:57:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# 切换源码到官方OpenWrt 22.03稳定分支
+cd openwrt
+git checkout openwrt-22.03
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加第三方插件源
+cat >> feeds.conf.default <<EOF
+src-git argon https://github.com/jerrykuku/luci-theme-argon.git^2.4.6
+src-git istore https://github.com/linkease/istore.git
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+EOF
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 将主题、应用商店复制进package锁定，防止更新覆盖
+cp -r feeds/argon/luci-theme-argon package/
+cp -r feeds/argon/luci-app-argon-config package/
+cp -r feeds/istore/luci-app-store package/
+
+# 注释第三方源，锁定版本不再拉新
+sed -i '/argon\|istore\|kenzo\|small/s/^/#/' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
