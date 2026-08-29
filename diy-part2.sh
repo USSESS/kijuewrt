481b328be08cc06d95d3b9072612a351d55f6ce7
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-30 02:00:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,13 +6,10 @@ sed -i 's|OpenWrt|KiJueWrt|g' package/base-files/files/etc/openwrt_release
 sed -i 's|DISTRIB_RELEASE=.*|DISTRIB_RELEASE='"'"'25.0.0.1'"'"'|g' package/base-files/files/etc/openwrt_release
 sed -i 's|DISTRIB_CODENAME=.*|DISTRIB_CODENAME='"'"'KiJue'"'"'|g' package/base-files/files/etc/openwrt_release
 sed -i 's|DISTRIB_DESCRIPTION=.*|DISTRIB_DESCRIPTION='"'"'KiJueWrt Built by GitHub Actions'"'"'|g' package/base-files/files/etc/openwrt_release
-
 # 修改默认LAN IP 10.10.10.1
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-
 # 修改主机名
 sed -i 's/set system.@system\[-1\].hostname=.*/set system.@system[0].hostname='\''KiJueWrt'\''/g' package/base-files/files/bin/config_generate
-
 # 写入uci-defaults：时区上海、中文、默认edge主题
 cat > package/base-files/files/etc/uci-defaults/99-custom <<EOF
 uci set system.@system[0].timezone='Asia/Shanghai'
@@ -21,7 +18,6 @@ uci set luci.main.mediaurlbase='/luci-static/edge'
 uci commit
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-custom
-
 # ========== SSH Banner ==========
 cat > package/base-files/files/etc/banner <<"BANNEREOF"
 ░██     ░██ ░██    ░█████                       ░██       ░██             ░██
@@ -35,5 +31,4 @@ cat > package/base-files/files/etc/banner <<"BANNEREOF"
              KiJueWrt 25.0.0.1
 =========================================================
 BANNEREOF
-
 echo "✅ diy-part2 KiJueWrt全部设置完成"
