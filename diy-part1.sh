34f0065229cf4bd3275d41e9f92eeeecce831645
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-29 22:44:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# 删除 set‑e !!!
 
-cp -r "$GITHUB_WORKSPACE/package" ./
+#在线拉取istore整套源码，直接进openwrt/package
+git clone https://github.com/linkease/istore.git temp_istore
+mv temp_istore/luci/* ./package/
+rm -rf temp_istore
 
+#复制edge主题
 if [ -d "$GITHUB_WORKSPACE/luci-theme-edge-master" ];then
     cp -r "$GITHUB_WORKSPACE/luci-theme-edge-master" package/luci-theme-edge
     echo "OK:Edge主题复制完成"
@@ -10,6 +13,9 @@ else
     echo "ERROR:缺失edge主题"
     exit 1
 fi
+
+#复制你仓库剩下的其他插件 netwizard fwx系列
+cp -r "$GITHUB_WORKSPACE/package/"* ./package/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
