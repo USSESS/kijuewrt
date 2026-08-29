a429539860b75eb32c40a1cbf9731d5920327941
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-29 22:32:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 set -e
 
-#复制仓库全部package插件到openwrt编译目录
 cp -r "$GITHUB_WORKSPACE/package" ./
 
-#复制edge主题
 if [ -d "$GITHUB_WORKSPACE/luci-theme-edge-master" ];then
     cp -r "$GITHUB_WORKSPACE/luci-theme-edge-master" package/luci-theme-edge
-    echo "✅本地Edge主题已导入"
+    echo "OK:Edge主题复制完成"
 else
-    echo "❌错误：本地Edge主题文件缺失！终止编译"
+    echo "ERROR:缺失edge主题"
     exit 1
 fi
 
-#更新feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-echo "====== 检查本地内置包 ======"
-PACKAGES=(
+echo "===开始校验本地包==="
+PACK=(
 luci-app-store
 luci-lib-taskd
 luci-lib-xterm
 taskd
 luci-app-netwizard
 )
-
-for p in "${PACKAGES[@]}"; do
-    if [ ! -d "package/$p" ];then
-        echo "!!! package/$p 【缺失】！终止编译！"
-        ls -la package/
-        exit 1
-    fi
-    echo "OK package/$p 存在"
+for p in "${PACK[@]}";do
+  if [ ! -d "package/$p" ];then
+    echo "MISS: $p"
+    ls -la package/
+    exit 1
+  fi
+  echo "OK:$p"
 done
 
-echo "====== diy‑part1.sh 执行完毕 ======"
+echo "diy-part1.sh全部执行完成"
